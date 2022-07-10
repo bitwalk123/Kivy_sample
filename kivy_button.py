9ab68ae2ba95a2c6e4962c4934f2d8ebfe6eb3b9
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-07-11 08:59:36 +0900

    init

---
+++ kivy_button.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+# Reference:
+# https://kivy.org/doc/stable/api-kivy.uix.button.html
+class KivyButton(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        btn = Button(text='Hello, World!')
+        btn.bind(on_press=self.on_button_pressed)
+        self.add_widget(btn)
+
+    def on_button_pressed(self, instance):
+        print('The button <%s> is being pressed' % instance.text)
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'HelloWorld'
+        return KivyButton()
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
