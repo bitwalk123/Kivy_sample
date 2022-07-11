045a56f5e0c5cbd30ffd010c8e8b6a330ebc5821
Fuhito Suguri <bitwalk@gmail.com>
2022-07-11 20:36:33 +0900

    update

--- kivy_button.py
+++ kivy_button.py
@@ -13,7 +13,7 @@ Window.size = (200, 200)
 class KivyButton(BoxLayout):
     def __init__(self):
         super().__init__()
-        self.orientation='vertical'
+        self.orientation = 'vertical'
         self.init_ui()
 
     def init_ui(self):
