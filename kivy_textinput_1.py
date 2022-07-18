264f558d7f396497b8113226e828e6433ee43c49
Keiichi Takahashi <bitwalk@gmail.com>
2022-07-18 15:28:46 +0900

    update

--- kivy_textinput_1.py
+++ kivy_textinput_1.py
@@ -3,14 +3,24 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
 
 Builder.load_file('kivy_textinput_1.kv')
 Window.size = (200, 200)
 
 
 class KivyTextInput(BoxLayout):
-    pass
+    def on_focus(self, instance, value):
+        if value:
+            print('%s がフォーカスされました。' % instance)
+        else:
+            print('%s のフォーカスが外れました。' % instance)
+
+    def on_enter(self, instance):
+        print('%s で改行キーが押されました。' % instance)
+
+    def on_text(self, instance, value):
+        print('ウィジェット %s の入力:\n%s' % (instance, value))
+
 
 class ExampleApp(App):
     def build(self):
