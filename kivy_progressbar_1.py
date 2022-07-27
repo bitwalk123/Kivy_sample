9089976178dbf12c206e162169d731e145935b76
Fuhito Suguri <bitwalk@gmail.com>
2022-07-28 07:53:36 +0900

    update

---
+++ kivy_progressbar_1.py
@@ -0,0 +1,35 @@
+import threading
+import time
+
+import japanize_kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file('kivy_progressbar_1.kv')
+Window.size = (200, 200)
+
+
+def counter(progress):
+    for c in range(100):
+        time.sleep(0.1)
+        progress.value = c
+
+
+class KivyProgressBar(BoxLayout):
+    pbar = ObjectProperty(None)
+
+    def demo(self, instance):
+        threading.Thread(target=counter, args=(self.pbar,), daemon=True).start()
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'ProgressBar'
+        return KivyProgressBar()
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
