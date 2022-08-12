1c9507bcb544f4735c16242570d5cf7ee2a66599
Keiichi Takahashi <bitwalk@gmail.com>
2022-08-13 07:55:27 +0900

    update

---
+++ kivy_pagelayout_2.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.uix.pagelayout import PageLayout
+
+
+
+class KivyPageLayout(PageLayout):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        img_1 = Image(source='image_1.jpg')
+        self.add_widget(img_1)
+        img_2 = Image(source='image_2.jpg')
+        self.add_widget(img_2)
+        img_3 = Image(source='image_3.jpg')
+        self.add_widget(img_3)
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'PageLayout'
+        return KivyPageLayout()
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
