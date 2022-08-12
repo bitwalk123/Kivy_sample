969208bb2aa5720633e8946cbec3a6363847d3c3
Keiichi Takahashi <bitwalk@gmail.com>
2022-08-12 09:39:02 +0900

    update

---
+++ kivy_anchorlayout.py
@@ -0,0 +1,36 @@
+import japanize_kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+
+Window.size = (200, 200)
+
+
+class KivyButton(AnchorLayout):
+    def __init__(self):
+        super().__init__()
+        self.anchor_x = 'right'
+        self.anchor_y = 'bottom'
+        self.init_ui()
+
+    def init_ui(self):
+        btn_a = Button(text='ボタンＡ', size_hint=(.67, .67))
+        btn_a.bind(on_press=self.on_button_pressed)
+        self.add_widget(btn_a)
+        btn_b = Button(text='ボタンＢ', size_hint=(.33, .33))
+        btn_b.bind(on_press=self.on_button_pressed)
+        self.add_widget(btn_b)
+
+    def on_button_pressed(self, instance):
+        print('%s がクリックされました。' % instance.text)
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'AnchorLayout'
+        return KivyButton()
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
