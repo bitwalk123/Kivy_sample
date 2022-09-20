191bd048cabd1551926dc0e58b0847dd91ff9269
Fuhito Suguri <bitwalk@gmail.com>
2022-09-21 06:16:05 +0900

    update

---
+++ kivy_relativelayout_3.py
@@ -0,0 +1,29 @@
+import sys
+
+import kivy
+import japanize_kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.relativelayout import RelativeLayout
+
+Builder.load_file('kivy_relativelayout_3.kv')
+Window.size = (200, 200)
+
+
+class KivyRelativeLayout(RelativeLayout):
+    pass
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'RelativeLayout'
+        return KivyRelativeLayout()
+
+
+if __name__ == '__main__':
+    # version information
+    print(sys.version)
+    print(kivy.__version__)
+    #
+    ExampleApp().run()
