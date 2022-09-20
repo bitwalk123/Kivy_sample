98abd467eab110721280e652c2c56fd3db782a2e
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-09-20 16:16:54 +0900

    update

---
+++ kivy_stacklayout_1.py
@@ -0,0 +1,23 @@
+import japanize_kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.stacklayout import StackLayout
+
+Builder.load_file('kivy_stacklayout_1.kv')
+Window.size = (200, 200)
+
+
+class KivyStackLayout(StackLayout):
+    def on_button_pressed(self, instance):
+        print('ボタン %s がクリックされました。' % instance.text)
+
+
+class ExampleApp(App):
+    def build(self):
+        self.title = 'StackLayout'
+        return KivyStackLayout()
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
